1e201448ef4aa644735393a086220631d8fc6660
Oleksandr Khomyakov <oleksandr.khomyakov@devico.io>
2024-06-25 00:53:25 +0300

    Some refactorings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,46 +18,80 @@ const Home = () => {
   const handleExpandCollapse = () => {
     setIsExpanded(!isExpanded);
   };
+
   return (
     <>
       <h1 className="text-2xl font-bold">Where is my note?</h1>
       <div className="border-gray-300 rounded-lg min-w-full md:min-w-[650px] lg:min-w-[1024px]">
         <div className="flex items-center">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search..."
-            className={clsx(
-              'flex-1 p-2 border border-gray-300',
-              searchQuery ? 'rounded-l-lg' : 'rounded-lg',
-            )}
+          <SearchInput
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
           />
-          {searchQuery && (
-            <button
-              onClick={handleClear}
-              className="p-2 bg-white hover:bg-gray-300 border border-l-0 rounded-r-lg border-gray-300"
-            >
-              <LuXCircle size={24} />
-            </button>
-          )}
-          <button
-            onClick={handleExpandCollapse}
-            className="ml-2 bg-blue-500 p-2 text-white rounded-full hover:bg-blue-600"
-          >
-            <LuStickyNote color="white" size={24} />
-          </button>
+          <ClearButton searchQuery={searchQuery} handleClear={handleClear} />
+          <ExpandCollapseButton handleExpandCollapse={handleExpandCollapse} />
         </div>
-        {isExpanded && (
-          <div className="mt-4 p-4 bg-gray-100 border border-gray-300 rounded-lg">
-            <NoteForm />
-          </div>
-        )}
+        <NoteFormContainer isExpanded={isExpanded} />
       </div>
 
       <NoteList searchQuery={debouncedSearchQuery} />
     </>
   );
 };
+
+const SearchInput = ({
+  searchQuery,
+  setSearchQuery,
+}: {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+}) => (
+  <input
+    type="text"
+    value={searchQuery}
+    onChange={(e) => setSearchQuery(e.target.value)}
+    placeholder="Search..."
+    className={clsx(
+      'flex-1 p-2 border border-gray-300',
+      searchQuery ? 'rounded-l-lg' : 'rounded-lg',
+    )}
+  />
+);
+
+const ClearButton = ({
+  searchQuery,
+  handleClear,
+}: {
+  searchQuery: string;
+  handleClear: () => void;
+}) =>
+  searchQuery && (
+    <button
+      onClick={handleClear}
+      className="p-2 bg-white hover:bg-gray-300 border border-l-0 rounded-r-lg border-gray-300"
+    >
+      <LuXCircle size={24} />
+    </button>
+  );
+
+const ExpandCollapseButton = ({
+  handleExpandCollapse,
+}: {
+  handleExpandCollapse: () => void;
+}) => (
+  <button
+    onClick={handleExpandCollapse}
+    className="ml-2 bg-blue-500 p-2 text-white rounded-full hover:bg-blue-600"
+  >
+    <LuStickyNote color="white" size={24} />
+  </button>
+);
+
+const NoteFormContainer = ({ isExpanded }: { isExpanded: boolean }) =>
+  isExpanded && (
+    <div className="mt-4 p-4 bg-gray-100 border border-gray-300 rounded-lg">
+      <NoteForm />
+    </div>
+  );
 
 export default Home;
--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -10,9 +10,12 @@ const NoteDetail = () => {
 
   return (
     <div className="min-w-full md:min-w-[650px] lg:min-w-[1024px] space-y-5">
-          <Link to={"/"} className="bg-green-600 hover:bg-green-700 p-2 pl-10 pr-10 text-white rounded-full">
+      <Link
+        to={'/'}
+        className="bg-green-600 hover:bg-green-700 p-2 pl-10 pr-10 text-white rounded-full"
+      >
         Go Back
-    </Link>
+      </Link>
       <NoteForm note={note} />
     </div>
   );
