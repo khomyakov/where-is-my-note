b8d52e51014a5e785c88dedd60d414daec811188
Oleksandr Khomyakov <oleksandr.khomyakov@devico.io>
2024-06-24 19:18:11 +0300

    Devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRouter from './router.tsx';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +11,7 @@ const App = () => {
       <div className="min-h-screen bg-gray-100 p-4">
         <AppRouter />
       </div>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
