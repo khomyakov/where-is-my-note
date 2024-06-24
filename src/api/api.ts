e4c7541c7cfd044a6a3aaefec102190cf3df6b4f
Oleksandr Khomyakov <oleksandr.khomyakov@devico.io>
2024-06-24 17:58:04 +0300

    Added API

---
+++ src/api/api.ts
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/oleksandr-devico/where-is-my-note',
+});
+
+export const fetchNotes = async () => {
+  const response = await api.get('/notes');
+  return response.data;
+};
+
+export const fetchNoteById = async (id: string) => {
+  const response = await api.get(`/notes/${id}`);
+  return response.data;
+};
+
+export const createNote = async (note: { title: string; content: string }) => {
+  const response = await api.post('/notes', note);
+  return response.data;
+};
+
+export const updateNote = async (id: string, note: { title: string; content: string }) => {
+  const response = await api.put(`/notes/${id}`, note);
+  return response.data;
+};
+
+export const deleteNote = async (id: string) => {
+  await api.delete(`/notes/${id}`);
+};
